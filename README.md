0fd43f975a659472ce95855b56649c36c024f8e7
Vicki Boykis <vicki.boykis@gmail.com>
2016-08-29 15:18:11 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java the Hard Way
+
+Going through all of the exercises in [Learn Java the Hard Way](https://learnjavathehardway.org/book/). 
---
+++ src/main/java/com/vickiboykis/javahard/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      WAWA              |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2016-08-29  01:20PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       10.870  |" );
+		System.out.println( "| Price/gallon: $ 2.23   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 24.24   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
